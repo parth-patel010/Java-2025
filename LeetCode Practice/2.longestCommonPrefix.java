dc007df4c9bc6f5e6f57d1e6411d5d6547a54a5c
Parth_Patel__. <parthp12321@gmail.com>
2025-07-05 04:19:03 +0530

    longestCommonPrefix 14

--- LeetCode Practice/2.longestCommonPrefix.java
+++ LeetCode Practice/2.longestCommonPrefix.java
@@ -1,20 +1,23 @@
+// This is class named Solution which matchest leetcode file name because class should match with file name
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if(strs == null || strs.length == 0) {
-            return "";
+    // we are created method with public access type and return type is string the passing parameters are list of array string named scrs
+    public String longestCommonPrefix(String[] srcs){
+        // Checking if input is empty, if condition is true it will return empty string
+        if(srcs == null || srcs.length == 0){
+            return " ";
         }
+        String prefix = srcs[0] // Flow
 
-        String prefix = strs[0];
-
-        for(int i = 1; i < strs.length; i++) {
-            while(strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length() - 1);
-                if(prefix.isEmpty()) {
-                    return "";
+        // Using for loop to checking prefix one by one
+        for(int i = 1;i<srcs.length;i++){
+            while(srcs[i].indexOf(prefix) != 0){
+                prefix = prefix.substring(0, prefix.length() - 1 ); // Flowe
+                // If there is no prefix it will return empty string
+                if(prefix.isEmpty()){
+                return "";
                 }
             }
         }
-
-        return prefix;
+        return prefix; // If fl = fl by checking it will return prefix is fl
     }
 }
